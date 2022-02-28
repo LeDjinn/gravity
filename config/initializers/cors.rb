0a5a7154da4cd0d6824dbf83b7e6379bab5da29a
LeDjinn <timour.spiridonov@gmail.com>
2022-02-28 23:36:03 +0100

    version cookies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get,:put, :patch, :delete, :head, :post, :options],
+    credentials: true
+    end
+
+    allow do
         origins 'https://upbeat-neumann-fefc86.netlify.app'
         resource '*', headers: :any, methods: [:get,:put, :patch, :delete, :head, :post, :options],
     credentials: true
