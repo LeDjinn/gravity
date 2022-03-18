5f0f6252e222d724cd8c4763dd42816f5fd2f876
LeDjinn <timour.spiridonov@gmail.com>
2022-03-18 19:03:47 +0100

    add same_site cookie 2.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_gravity', same_site: :none, secure: true, domain: 'https://gravitytestapi.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_gravity',  domain: 'https://gravitytestapi.herokuapp.com'
 else
-    Rails.application.config.session_store :cookie_store, key: "_gravity",same_site: :none, secure: true, domain: 'https://gravitytestapi.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: "_gravity"
 end
